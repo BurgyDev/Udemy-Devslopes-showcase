c6982ac055cfc8bdd061295ee2bd05dd51cf58de
Yong Jae Kim <yongjaekim@Yongui-MacBook-Pro.local>
2015-11-29 23:57:21 +0900

    first commit

---
+++ MaterialView.swift
@@ -0,0 +1,20 @@
+//
+//  MaterialView.swift
+//  burgydev-showcase
+//
+//  Created by Yong Jae Kim on 2015. 11. 29..
+//  Copyright © 2015년 Yong Jae Kim. All rights reserved.
+//
+
+import UIKit
+
+class MaterialView: UIView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        self.layer.cornerRadius = 2.0
+        // self.layer.shadowColor
+    }
+
+}
